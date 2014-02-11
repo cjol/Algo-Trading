642d8ce6d8f36b4af6dbed1b4dce56fae9b05f7a
Artjoms Iskovs <ai280@cam.ac.uk>
2014-02-11 14:17:11 +0000

    Added Javadoc comments to time series objects
    (except for HighestBid and LowestOffer)

--- src/valueObjects/ArrayValueObject.java
+++ src/valueObjects/ArrayValueObject.java
@@ -1,11 +1,16 @@
 package valueObjects;
 
-/*
+
+/**
  * A wrapper around a basic array to represent it as a value object.
  */
 public class ArrayValueObject implements IValued {
 	private int[] array;
 	
+	/**
+	 * Creates a time series object out of an array.
+	 * @param array The array to convert into a value object. The last element of the array is considered to be the current one.
+	 */
 	public ArrayValueObject(int[] array) {
 		this.array = array;
 	}
