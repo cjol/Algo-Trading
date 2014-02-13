dcfa4800381d91cae940c4677bcbf08680729ab0
Artjoms Iskovs <mildbyte@gmail.com>
2014-02-13 16:00:47 +0000

    Fixed the unit test.

--- src/unitTests/DatabaseToReconstructorTest.java
+++ src/unitTests/DatabaseToReconstructorTest.java
@@ -2,7 +2,9 @@ package unitTests;
 
 import java.sql.SQLException;
 import java.sql.Timestamp;
+import java.util.Iterator;
 
+import orderBookReconstructor.Match;
 import orderBookReconstructor.OrderBookReconstructor;
 
 import org.junit.Test;
@@ -19,9 +21,20 @@ public class DatabaseToReconstructorTest {
 		DatasetHandle dataset = dh.getDataset("test");
 		StockHandle stockHandle = dh.getAllStocks(dataset).get(0);
 		
+		//NB: timestamp constructor wants the year to be 1900-based,
+		//day to be 1-based and month to be 0-based.
 		OrderBookReconstructor obr = new OrderBookReconstructor(
-				new Timestamp(2014, 1, 1, 0, 0, 0, 0), stockHandle, dh);
+				new Timestamp(114, 0, 1, 0, 0, 0, 0), stockHandle, dh);
 		
+		Iterator<Match> matches = obr.updateTime(new Timestamp(114, 0, 1, 0, 0, 5, 0));
+		
+		while (matches.hasNext()) {
+			Match m = matches.next();
+			
+			System.out.println("MATCH: BUY " + m.buyOrder.getVolume() + " at " + m.buyOrder.getPrice()
+					+ " AND SELL " + m.sellOrder.getVolume() + " at " + m.sellOrder.getPrice() + 
+					" (on " + m.quantity + " items)");
+		}
 	}
 
 }
