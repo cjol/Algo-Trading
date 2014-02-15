61707c8b2c05f396cbbc45b2a6290f9b06caa168
nickburscough <nb455@cam.ac.uk>
2014-02-15 21:26:33 +0000

    Added TODOs for tomorrow. Should be done soon.

--- src/dataImporter/DataImporter.py
+++ src/dataImporter/DataImporter.py
@@ -19,13 +19,21 @@ databaseUser = raw_input("Username: ")
 databasePassword = getpass.getpass()
 databaseName = raw_input("Database Name: ")
 db = MySQLdb.connect(databaseURL, databaseUser, databasePassword, databaseName)
+cursor = db.cursor()
+cursor.execute('INSERT INTO datasets(dataset_id,name) VALUES (0,"ADS");')
 
 file = h5py.File(hdf5_file_name, 'r')   # 'r' means that hdf5 file is open in read-only mode
 dataset = file[dataset_name]
-for a in dataset:
-    print a
+#TODO:formattedDataset = <Array of tuples of form (int,string,timestamp,ask/buy,int,int)>
+#TODO:Transform dataset to formattedDataset
+
+for data in dataset:
+    print data
     #TODO: insert into database
-    
+
 file.close()
 db.close()
 
+#example insert - need to change format of data to match database schema
+#INSERT INTO trades(dataset_id,ticker,ts,bid_or_ask,price,volume) VALUES (0,"FOO","2014-01-01 00:00:00","B",9,200);
+#INSERT INTO trades(dataset_id,ticker,ts,bid_or_ask,price,volume) VALUES (0,"BAR","2014-01-01 00:00:00","B",9,200);
