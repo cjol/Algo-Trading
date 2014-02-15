1e7cbaa8bfc94b5f9460950668b588b55ec163db
nickburscough <nb455@cam.ac.uk>
2014-02-15 21:01:19 +0000

    Added prompting database connection

--- src/dataImporter/DataImporter.py
+++ src/dataImporter/DataImporter.py
@@ -1,18 +1,31 @@
 #!/usr/bin/python
 import h5py
 import pandas as pd
+import MySQLdb
+from sql import *
+from sql.aggregate import *
+from sql.conditionals import *
+import getpass
 
-#f = h5py.File("./ADS.h5", 'r')
-#d = pd.DataFrame(f[type].value)
-#d.set_index('timestamp', drop=True, inplace=True, verify_integrity=False)
 
+#setting up hdf5 variables
 hdf5_file_name = './ADS.h5'
 dataset_name = 'RetailStates'
 event_number = 0
+
+#setting up database connection
+databaseURL = raw_input("Database URL: ")
+databaseUser = raw_input("Username: ")
+databasePassword = getpass.getpass()
+databaseName = raw_input("Database Name: ")
+db = MySQLdb.connect(databaseURL, databaseUser, databasePassword, databaseName)
 
 file = h5py.File(hdf5_file_name, 'r')   # 'r' means that hdf5 file is open in read-only mode
 dataset = file[dataset_name]
 for a in dataset:
     print a
+    #TODO: insert into database
+    
 file.close()
+db.close()
 
