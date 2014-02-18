689b3b877b5048d6cd6baf84b66596cddde2b4b0
alanrusnak9 <alanrusnak9@gmail.com>
2014-02-18 11:56:21 +0000

    Commit 2/4 for Results

---
+++ src/testHarness/output/ListDataResult.java
@@ -0,0 +1,24 @@
+package testHarness.output;
+
+import java.util.List;
+
+public class ListDataResult extends Result {
+
+	private List data;
+	
+	public ListDataResult(List data){
+		this.data = data;
+	}
+	
+	public void outputToFile(String location){
+		// TODO
+	}
+	
+	public String outputToString(){
+		//TODO
+		return null;
+	}
+	
+	
+	
+}
