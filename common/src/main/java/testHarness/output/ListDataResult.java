a53ea540e8927d8c081af5455e5bb2179845922f
Christopher Little <hephistocles@gmail.com>
2014-02-20 11:32:20 +0000

    Implement methods in ListDataResult

--- common/src/main/java/testHarness/output/ListDataResult.java
+++ common/src/main/java/testHarness/output/ListDataResult.java
@@ -9,14 +9,35 @@ public class ListDataResult extends Result {
 	public ListDataResult(List data){
 		this.data = data;
 	}
-	
+
+	/**
+	 * Outputs the list with each value on a new line to the specified file 
+	 * location (calling toString() on each datum)
+	 * @param location desired file location
+	 */
 	public void outputToFile(String location){
-		// TODO
+		//TODO This just prints each value on a new line. May like other formats (CSV, etc)
+		PrintWriter out = new PrintWriter(location);
+		for (Object datum : data) {
+			out.println(datum.toString());
+		}
 	}
 	
+	/**
+	 * Outputs the list as a comma-separated string of values (calling toString on each datum)
+	 * @return A comma-separated string of values.
+	 */
 	public String outputToString(){
-		//TODO
-		return null;
+		String prefix = "";
+		String result = "";
+		for (Object datum : data) {
+			result += prefix;
+			result += datum.toString();
+			if (prefix.isEmpty()) {
+				prefix = ",";
+			}
+		}
+		return result;
 	}
 	
 	
