26256284ac5ba2f34d27d5009f46682ffe33779a
A.A. Rusnak <aar39@cam.ac.uk>
2014-02-21 13:45:10 +0000

    Fixed TimeTaken

--- common/src/main/java/testHarness/output/TimeTaken.java
+++ common/src/main/java/testHarness/output/TimeTaken.java
@@ -1,18 +1,17 @@
 package testHarness.output;
 
-import java.math.BigDecimal;
 import java.sql.Timestamp;
 import java.util.HashMap;
-import java.util.LinkedList;
-import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
+
+import org.json.JSONObject;
 
 import database.OutputServer;
 import testHarness.TickData;
 import testHarness.output.Output;
 import testHarness.output.result.Result;
-import testHarness.output.result.SingletonResult;
+
 
 public class TimeTaken extends Output{
 	
@@ -27,10 +26,9 @@ public class TimeTaken extends Output{
 
 	@Override
 	public Result getResult() {
-		Map<String, Result> resultMap = new HashMap<String,Result>();
+		JSONObject resultMap = new JSONObject();
 		for (Entry<Timestamp, Long> timetakenDataPoint : timeTakenData.entrySet()) {
-			resultMap.put(timetakenDataPoint.getKey().toString(), 
-					new SingletonResult(timetakenDataPoint.getValue()));
+			resultMap.put(timetakenDataPoint.getKey().toString(),timetakenDataPoint.getValue().longValue());
 		}
 		Result result = new Result(resultMap);
 		if(outputServer != null) outputServer.store(result);
