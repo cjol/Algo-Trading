9f07dc1aaa734cba2027d97b4c826301212fa45f
Artjoms Iskovs <ai280@cam.ac.uk>
2014-03-05 13:43:15 +0000

    BBands plotting for the grapher

--- plot_json.py
+++ plot_json.py
@@ -8,8 +8,9 @@ Created on Thu Feb 27 15:33:56 2014
 import json
 from datetime import datetime
 
-from matplotlib.pyplot import plot_date, plot, vlines, legend, gca, ion
+from matplotlib.pyplot import plot_date, plot, vlines, legend, gca
 from pylab import movavg
+from numpy import std
 
 def decode_series(path):
     series = json.JSONDecoder().decode(open(path).read())
@@ -22,10 +23,10 @@ def decode_series(path):
 def plot_all():
     plot_bah()
     
-    t,v2 = decode_series("/home/mildbyte/git/Algo-Trading/portfolioValue.json")
-    t,v1 = decode_series("/home/mildbyte/git/Algo-Trading/availableFunds.json")
+    t,v2 = decode_series("portfolioValue.json")
+    t,v1 = decode_series("availableFunds.json")
 
-    t,p = decode_series("/home/mildbyte/git/Algo-Trading/portfolio.json")
+    t,p = decode_series("portfolio.json")
    
     changed_t = []    
     for currt, currp, prevp, currv, prevv in zip(t, p[1:], p, v1[1:], v1):
@@ -42,14 +43,14 @@ def plot_json(path):
     plot_date(t,v, 'b-')
 
 def plot_account():
-    t,v2 = decode_series("/home/mildbyte/git/Algo-Trading/portfolioValue.json")
-    t,v1 = decode_series("/home/mildbyte/git/Algo-Trading/availableFunds.json")
+    t,v2 = decode_series("portfolioValue.json")
+    t,v1 = decode_series("availableFunds.json")
     
     plot_date(t, [a+b for a,b in zip(v1,v2)], 'r-')
 
 def plot_bah():
-    t,v2 = decode_series("/home/mildbyte/git/Algo-Trading/portfolioValue_BAH.json")
-    t,v1 = decode_series("/home/mildbyte/git/Algo-Trading/availableFunds_BAH.json")
+    t,v2 = decode_series("portfolioValue_BAH.json")
+    t,v1 = decode_series("availableFunds_BAH.json")
     
     plot_date(t, [a+b for a,b in zip(v1,v2)], 'k-')
 
@@ -62,10 +63,39 @@ def plot_portfolio():
     plot(t,v)
 
 def plot_averages(sWnd, fWnd):
-    t,v1 = decode_series("/home/mildbyte/git/Algo-Trading/availableFunds_BAH.json")
-    t,v2 = decode_series("/home/mildbyte/git/Algo-Trading/portfolioValue_BAH.json")
+    t,v1 = decode_series("availableFunds_BAH.json")
+    t,v2 = decode_series("portfolioValue_BAH.json")
     vs = [a+b for a,b in zip(v1,v2)]
     slow = movavg(vs, sWnd)
     fast = movavg(vs, fWnd)
-    plot(t[fWnd-1:], fast, 'r-')
+    plot(t[fWnd-1:], fast, 'b-')
     plot(t[sWnd-1:], slow, 'g-')
+
+def rolling_stdev(arr, window):
+    wnd = arr[:window]
+    result = []
+    
+    for i in range(len(arr) - window):
+        result.append(std(wnd))
+        wnd = wnd[1:]
+        wnd.append(arr[i + window])
+    
+    result.append(std(wnd))
+    
+    return result
+
+def plot_bbands(window, width):
+    t,v1 = decode_series("availableFunds_BAH.json")
+    t,v2 = decode_series("portfolioValue_BAH.json")
+    vs = [a+b for a,b in zip(v1,v2)]
+    
+    stdevs = rolling_stdev(vs, window)
+    mvavg = movavg(vs, window)
+    
+    upper_band = [a + width * b for a,b in zip(mvavg, stdevs)]
+    lower_band = [a - width * b for a,b in zip(mvavg, stdevs)]
+    
+    plot(t[window-1:], mvavg, 'b-')
+    plot(t[window-1:], upper_band, 'b--')
+    plot(t[window-1:], lower_band, 'b--')
+    
